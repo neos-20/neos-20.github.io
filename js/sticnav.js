e9d56628152b9aa6877412525e043b393d45f3c2
mohit <mvk1407@gmail.com>
2021-08-17 17:14:14 +0530

    new footer layout

--- js/sticnav.js
+++ js/sticnav.js
@@ -2,34 +2,26 @@ window.onscroll = function () {
     scrollfunc()
     stickynavfunc()
 };
-
-
-
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
-
-
-function stickynavfunc(){
-    if(window.pageYOffset >= sticky){
+function stickynavfunc() {
+    if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
 
-    }
-    else{
+    } else {
         navbar.classList.remove("sticky");
     }
 }
 
 
 
-function scrollfunc(){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
-        
+function scrollfunc() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+
         document.getElementById("navbar").style.padding = "0px 0px";
 
-    }
-    else{
+    } else {
         document.getElementById("navbar").style.padding = "20px 0px";
-  
+
     }
 }
-
